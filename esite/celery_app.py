419d1fe1ccbcc63bd812caf1e939fd14d65636bc
longwand <longwand@schett.net>
2021-02-26 21:08:27 +0100

    Fix celery_settings
    The config_from_object string has been fixed.

--- esite/celery_app.py
+++ esite/celery_app.py
@@ -8,5 +8,5 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "esite.settings.production")
 
 app = Celery("esite")
-app.config_from_object("django.conf.settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
